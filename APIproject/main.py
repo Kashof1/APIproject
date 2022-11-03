193d85b3025ebd09905694d39102aff94f562809
Kashof1 <theibster6@gmail.com>
2022-11-03 14:51:34 +0000

    fixed the uvicorn file

--- APIproject/main.py
+++ APIproject/main.py
@@ -20,9 +20,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def root():
-info.log(“gone to root”)
-return {"message": "Hello World"}
+    log.info("gone to root")
+    return {"message": "Hello World"}
 @app.get("/items/{item_id}")
 async def read_item(item_id):
-info.warning(f“loaded item {item_id}”)
-return {"item_id": item_id}
+    log.warning(f"loaded item {item_id}")
+    return {"item_id": item_id}
