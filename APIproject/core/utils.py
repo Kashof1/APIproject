3396b18705a026b8985d7dd3f190181d6b6331c5
Ibrahim Kashif <94566109+Kashof1@users.noreply.github.com>
2022-11-11 14:15:58 +0000

    utils

---
+++ APIproject/core/utils.py
@@ -0,0 +1,12 @@
+import logging
+import urllib.request
+
+log = logging.getLogger(__name__)
+
+def get_url(url:str):
+    req_info = urllib.request.urlopen(url)
+    log.info(f"REQUESTED DATA FROM {url}")
+    data = req_info.read()
+    datastr = data.decode("utf8")
+    req_info.close
+    return datastr
